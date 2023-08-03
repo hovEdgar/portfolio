d994cc181ffde0f1ef852c56dafa76c2d1605e64
edhov <edhov91@gmail.com>
2023-08-03 23:59:00 +0400

    have added font for progress section

---
+++ app/_document.js
@@ -0,0 +1,21 @@
+import {Head, Html, Main, NextScript} from 'next/document'
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com"/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Gasoek+One&family=Lato&family=Quicksand:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+			<Main/>
+			<NextScript/>
+			<div id="root-modal"></div>
+			</body>
+		</Html>
+	)
+}
