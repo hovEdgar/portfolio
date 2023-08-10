07f600e8886f647b76c45fb80913792db467e94d
edhov <edhov91@gmail.com>
2023-08-10 23:47:51 +0400

    have cleaned up from unnessery font testings functionalities

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	// ...other config options...
-	webpack: (config, { isServer }) => {
-		// Add a rule to handle font files
-		config.module.rules.push({
-			test: /\.(woff|woff2|ttf)$/,
-			use: {
-				loader: 'file-loader',
-				options: {
-					publicPath: '/_next/static/fonts',
-					outputPath: 'static/fonts',
-					name: '[name].[ext]'
-				}
-			}
-		});
-
-		return config;
-	}
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
